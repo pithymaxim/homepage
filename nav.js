eea8f570c71c7c8390734be6f632a808d66cc7f0
pithymaxim <maximplaysbass@gmail.com>
2023-07-07 07:44:06 -0700

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,14 +1,2 @@
 
-document.write('<div>\
-<a href="http://maximmassenkoff.com/"> <h2>Maxim Massenkoff</h2> </a>\
-<hr/>\
- <p class="contactinfo"><a href="./#bio">Bio</a></p>\
-<p class="contactinfo"><a href="./#workingpapers">Working Papers</a></p>\
-<p class="contactinfo"><a href="./#publications">Publications</a></p>\
-<p class="contactinfo"><a href="./#workinprogress">Work in Progress</a></p>\
-<p class="contactinfo"><a href="./#teaching">Teaching</a></p>\
-<p class="contactinfo"><a href="./data.html">Data</a></p>\
-<p class="lastcontactinfo"><a href="https://scholar.google.com/citations?user=aYALu3oAAAAJ&hl=en">Google Scholar</a></p>\
-    </div>\
-    <!-- <img src="http://maximmassenkoff.com/images/maxim_pic.jpg" width="200px" alt="Photo" />\ -->
-');
+document.write('<a href="http://maximmassenkoff.com/"> <h2>Maxim Massenkoff</h2> </a> <p class="contactinfo"><a href="./#bio">Bio</a></p> <p class="contactinfo"><a href="./#workingpapers">Working Papers</a></p> <p class="contactinfo"><a href="./#publications">Publications</a></p> <p class="contactinfo"><a href="./#workinprogress">Work in Progress</a></p> <p class="contactinfo"><a href="./#teaching">Teaching</a></p> <p class="contactinfo"><a href="./data.html">Data</a></p> <p class="lastcontactinfo"><a href="https://scholar.google.com/citations?user=aYALu3oAAAAJ&hl=en">Google Scholar</a></p>');
