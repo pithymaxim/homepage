90f08c77ae9a527544989fe1ed5b821b71a43780
pithymaxim <pithymaxim@users.noreply.github.com>
2025-10-25 14:03:21 +0000

    deploy: 1cb0a8630fcd3c76daaa38ebae1c25d7b225bf9e

--- nav.js
+++ nav.js
@@ -1,2 +1,22 @@
 
-document.write('<a href="http://maximmassenkoff.com/"> <h2>Maxim Massenkoff</h2> </a> <p class="contactinfo"><a href="./#bio">Bio</a></p> <p class="contactinfo"><a href="./#workingpapers">Working Papers</a></p> <p class="contactinfo"><a href="./#publications">Publications</a></p> <p class="contactinfo"><a href="./#workinprogress">Work in Progress</a></p> <p class="contactinfo"><a href="./#teaching">Teaching</a></p> <p class="contactinfo"><a href="./data.html">Data</a></p> <p class="lastcontactinfo"><a href="https://scholar.google.com/citations?user=aYALu3oAAAAJ&hl=en">Google Scholar</a></p>');
+function loadNavigation() {
+    const nav = `
+        <a href="http://maximmassenkoff.com/"> <h2>Maxim Massenkoff</h2> </a>
+        <hr/>
+        <p class="contactinfo"><a href="http://maximmassenkoff.com/#bio">Bio</a></p>
+        <p class="contactinfo"><a href="http://maximmassenkoff.com/#publications">Publications</a></p>
+        <p class="contactinfo"><a href="http://maximmassenkoff.com/#workingpapers">Working Papers</a></p>
+        <p class="contactinfo"><a href="http://maximmassenkoff.com/#workinprogress">Work in Progress</a></p>
+        <p class="contactinfo"><a href="http://maximmassenkoff.com/#teaching">Teaching</a></p>
+        <p class="contactinfo"><a href="http://maximmassenkoff.com/#tools">Tools</a></p>
+        <p class="contactinfo"><a href="./data.html">Data</a></p>
+        <p class="contactinfo"><a href="https://scholar.google.com/citations?user=aYALu3oAAAAJ&hl=en">Google Scholar</a></p>
+        <p class="lastcontactinfo"><a href="https://apithymaxim.wordpress.com/">Blog</a></p>
+
+        <img src="http://maximmassenkoff.com/images/maxim_pic.png" width="200px" alt="Photo" />
+    `;
+    document.getElementById('navigation').innerHTML = nav;
+}
+
+// Load when page is ready
+document.addEventListener('DOMContentLoaded', loadNavigation);
